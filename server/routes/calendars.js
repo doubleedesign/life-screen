1de9fe672c09e35736e01da3b5db3143512f485e
Leesa Ward <leesa@doubleedesign.com.au>
2023-03-21 10:50:26 +1100

    feat(server): Separate routes for calendars and events

--- server/routes/calendars.js
+++ server/routes/calendars.js
@@ -1,10 +1,5 @@
-import addDays from 'date-fns/addDays';
-import formatISO from 'date-fns/formatISO';
-import zonedTimeToUtc from 'date-fns-tz/zonedTimeToUtc';
-import startOfWeek from 'date-fns/startOfWeek';
 import Router from 'express-promise-router';
 import graph from '../graph.js';
-import iana from 'windows-iana';
 const router = Router();
 
 router.get('/', async function (req, res) {
@@ -15,32 +10,7 @@ router.get('/', async function (req, res) {
 		return false;
 	}
 
-	const timezone = req.app.locals.cache.users[userId]['timeZone'];
-
-	// Initialise response object to add things into
-	let response = {};
-
-	// Convert user's Windows time zone (e.g. "Pacific Standard Time") to IANA format ("America/Los_Angeles")
-	const timeZoneId = iana.findIana(timezone);
-
-	// Calculate the start and end of the current week
-	// Get midnight on the start of the current week in the user's timezone,
-	// but in UTC. For example, for Pacific Standard Time, the time value would be 07:00:00Z
-	const weekStart = zonedTimeToUtc(startOfWeek(new Date()), timeZoneId.valueOf());
-	const weekEnd = addDays(weekStart, 7);
-
-	// Get calendars
 	const calendars = await graph.getCalendars(req.app.locals.cache.msalClient, userId);
-
-	// Get events
-	const events = graph.getCalendarView(
-		req.app.locals.cache.msalClient,
-		userId,
-		formatISO(weekStart),
-		formatISO(weekEnd),
-		timezone);
-
-	console.log(events);
 
 	res.status(200).send(calendars);
 
---
+++ server/routes/events.js
@@ -0,0 +1,51 @@
+import addDays from 'date-fns/addDays';
+import formatISO from 'date-fns/formatISO';
+import zonedTimeToUtc from 'date-fns-tz/zonedTimeToUtc';
+import startOfWeek from 'date-fns/startOfWeek';
+import Router from 'express-promise-router';
+import graph from '../graph.js';
+import iana from 'windows-iana';
+const router = Router();
+
+router.get('/', async function (req, res) {
+	const userId = Object.keys(req.app.locals.cache.users)[0];
+
+	if(!userId) {
+		res.status(404).send('User not found');
+		return false;
+	}
+
+	const timezone = req.app.locals.cache.users[userId]['timeZone'];
+
+	// Initialise response object to add things into
+	let response = {};
+
+	// Convert user's Windows time zone (e.g. "Pacific Standard Time") to IANA format ("America/Los_Angeles")
+	const timezoneId = iana.findIana(timezone);
+
+	// Calculate the start and end of the current week
+	// Get midnight on the start of the current week in the user's timezone,
+	// but in UTC. For example, for Pacific Standard Time, the time value would be 07:00:00Z
+	const weekStart = zonedTimeToUtc(startOfWeek(new Date()), timezoneId.valueOf()[0]);
+	const weekEnd = addDays(weekStart, 7);
+
+	// Get events
+	// This only gets the primary calendar.
+	// TODO: Get IDs of currently open/selected calendars and get their events
+	// GET https://graph.microsoft.com/v1.0/me/calendars/{id}/events
+	// https://learn.microsoft.com/en-us/graph/outlook-get-shared-events-calendars
+	/*
+	const events = await graph.getCalendarView(
+		req.app.locals.cache.msalClient,
+		userId,
+		formatISO(weekStart),
+		formatISO(weekEnd),
+		timezone);
+
+	console.log(events); */
+
+	res.status(200).send(response);
+
+});
+
+export default router;
