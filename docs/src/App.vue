fad09998bbd7bd4458370160583dbb710d1f743f
Leesa Ward <leesa@doubleedesign.com.au>
2023-12-10 22:34:17 +1100

    Docs: WIP

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 // @ts-ignore
+import Tag from './components/Tag.vue';
 import data from '../../server/spec.yaml';
 import { Spec } from './types.ts';
-const spec: Spec = data;
+const spec: Spec = data as Spec;
 </script>
 
 <template>
-    <h1>{{spec.info.title}}</h1>
-    <p>{{spec.info.description}}</p>
-    <h3 v-for="tag in spec.tags">{{tag.name}}</h3>
+    <header>
+        <h1>{{spec.info.title}}</h1>
+        <p>{{spec.info.description}}</p>
+    </header>
+    <template v-for="tag in spec.tags">
+        <Tag v-bind:heading="tag.name"/>
+    </template>
 </template>
 
 <style scoped lang="scss">
---
+++ docs/src/components/Tag.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+	props: {
+		heading: String,
+	}
+};
+</script>
+
+<template>
+    <section class="tag-content">
+        <header class="tag-content__header">
+            <h1>{{heading}}</h1>
+        </header>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    .tag-content {
+        &__header {
+            h1 {
+                font-size: 1.5rem;
+            }
+        }
+    }
+</style>
