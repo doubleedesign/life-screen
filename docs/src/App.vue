da5ecf1c64b47ae81176bd1f2fe59d384f5f4bf5
doubleedesign <leesa@doubleedesign.com.au>
2023-12-10 16:34:45 +1100

    Docs: The very beginnings of a simple docs app

---
+++ docs/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// @ts-ignore
+import data from '../../server/spec.yaml';
+import { Spec } from './types.ts';
+const spec: Spec = data;
+</script>
+
+<template>
+    <h1>{{spec.info.title}}</h1>
+    <p>{{spec.info.description}}</p>
+    <h3 v-for="tag in spec.tags">{{tag.name}}</h3>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ docs/src/types.ts
@@ -0,0 +1,46 @@
+type Tag = {
+	name: string;
+	description?: string;
+	externalDocs?: {
+		url: string
+	}
+}
+
+type Operation = 'get' | 'post' | 'patch' | 'put' | 'delete';
+
+type Parameter = {
+	name: string;
+	description: string;
+	in: string;
+	required: boolean;
+	schema?: {
+		[key: string]: any
+	}
+}
+
+type Responses = {
+	[code: number]: {
+		description: string;
+	}
+}
+
+export type Spec = {
+	info: {
+		title: string;
+		description: string;
+		version?: string;
+	}
+	openapi: string;
+	servers: { url: string }[];
+	tags: Tag[];
+	paths: {
+		[path: string]: {
+			[key in Operation]: {
+				summary: string;
+				tags: Pick<Tag, 'name'>[];
+				parameters: Parameter[];
+				responses: Responses
+			}
+		}
+	}
+}
