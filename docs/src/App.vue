5d22bbc4ed10dabe81ad7bd6e3ebd0c413f70554
Leesa Ward <leesa@doubleedesign.com.au>
2023-12-17 01:26:33 +1100

    Docs: WIP it good

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import Menu from './components/Menu.vue';
-import { useSpecStore } from './state/store';
-const spec = useSpecStore();
+import Drawer from './components/Drawer.vue';
+import PageHeader from './components/PageHeader.vue';
 </script>
 
 <template>
-    <header>
-        <h1>{{spec.info.title}}</h1>
-        <p>{{spec.info.description}}</p>
-    </header>
-    <Menu/>
-    <!-- route outlet -->
-    <router-view :key="$route.path"></router-view>
+    <div class="app-wrapper">
+        <Drawer>
+            <PageHeader/>
+        </Drawer>
+        <main class="page-content">
+            <!-- route outlet -->
+            <RouterView :key="$route.path"></RouterView>
+        </main>
+    </div>
 </template>
 
 <style scoped lang="scss">
+    .app-wrapper {
+        width: 100vw;
+        min-height: 100vh;
+        overflow: hidden;
+        display: flex;
+        flex-wrap: nowrap;
+        container-name: wrapper;
+        container-type: inline-size;
+    }
+    .page-content {
+        flex-grow: 1;
+        padding: 1rem;
+        overflow: scroll;
+        scroll-behavior: smooth;
+
+        @container wrapper (min-width: 1000px) {
+            padding: 1rem 2rem;
+        }
+    }
 </style>
---
+++ docs/src/components/Drawer.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export default {
+	props: {},
+	data() {},
+	computed: {},
+	methods: {}
+};
+</script>;
+
+<template>
+	<div class="drawer">
+		<slot></slot>
+	</div>
+</template>;
+
+<style scoped lang="scss">
+	.drawer {
+        width: 18rem;
+	}
+</style>;
