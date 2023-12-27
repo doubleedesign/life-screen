54aeb7f03e0a017ffdca52d9606d2aee00ca2bb0
doubleedesign <leesa@doubleedesign.com.au>
2023-12-27 17:32:29 +1100

    Docs: Update component generator template

---
+++ docs/templates/component/TemplateName.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'TemplateName',
+	props: {},
+	data() {
+		return {
+			content: 'TemplateName component',
+		};
+	},
+	computed: {},
+	methods: {}
+});
+</script>
+
+<template>
+	<div class="template-name">
+		{{ content }}
+	</div>
+</template>
+
+<style scoped lang="scss">
+.template-name {
+
+}
+</style>
